68716423b5c14213d8fb4464f0bccbda2f824638
Yazan-Daibes <yazan.daibes@yahoo.com>
2022-08-12 07:30:45 -0400

    My first File

--- rest.py
+++ rest.py
@@ -2,10 +2,18 @@ from app import app
 from flask import jsonify, request
 
 @app.route('/')
-def heelo():
-        resp = jsonify('Hello, I am Yazan Daibes!')
-        resp.status_code = 200
-        return resp
+def hello():
+#        resp = jsonify('Hello, I am Yazan Daibes!')
+#       resp.status_code = 200
+#        return resp
+	return 'Hello, I am Yazan Daibes!'
+
+
+@app.route('/how are you')
+def how_are_you():
+    
+	
+	return 'I am good, how about you?'
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0')
